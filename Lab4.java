2d3a646613427e559644d5adbd4f451a6cabefb4
Kannan Pillappan <knpk13@gmail.com>
2018-09-19 12:36:17 -0400

    Added sample Lab4 java file. Did not include entire code due to permissions errors on WSL

---
+++ Lab4.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Lab4 {
+ public static void main(String[] args) throws Exception
+ {
+  int i[] = {5,3,2,1,4};
+  Arrays.sort(i);
+  System.out.println(Arrays.toString(i));
+ }
+}
+
